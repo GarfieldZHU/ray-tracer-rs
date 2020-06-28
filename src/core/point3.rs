fadb868cfbcd09350b0fb6446cb891e338988e6d
jiazhu <garfield@bupt.edu.cn>
2020-06-28 10:18:25 +0800

    feat: add ray structure

--- src/core/point3.rs
+++ src/core/point3.rs
@@ -5,18 +5,29 @@ pub type Point3 = Vec3;
 #[cfg(test)]
 mod test {
   use super::Point3;
+  use super::Vec3;
 
-  pub fn test_point3_basic() {
+  #[test]
+  fn test_point3_basic() {
     let p = Point3::new(1.0, 2.0, 3.0);
     let q = Point3::new(3.0, 2.0, 1.0);
 
     assert_eq!(p + q, Point3::new(4.0, 4.0, 4.0));
   }
 
-  pub fn test_point3_dot() {
+  #[test]
+  fn test_point3_dot() {
     let p = Point3::new(1.0, 2.0, 3.0);
     let q = Point3::new(3.0, 2.0, 1.0);
 
     assert_eq!(Point3::dot(&p, &q), 10.0);
+  }
+
+  #[test]
+  fn test_add_vector() {
+    let p = Point3::new(1.0, 2.0, 3.0);
+    let r = Vec3::new(2.0, 2.0, 2.0);
+
+    assert_eq!(p + r, Point3::new(3.0, 4.0, 5.0));
   }
 }
--- src/core/ray.rs
+++ src/core/ray.rs
@@ -1,6 +1,41 @@
 use super::vec3::Vec3;
 use super::point3::Point3;
 
+#[derive(Debug)]
 pub struct Ray {
-  
+  pub origin: Point3,
+  pub direction: Vec3, 
+}
+
+impl Ray {
+  pub const fn new(origin: Point3, direction: Vec3) -> Self {
+    Self {
+      origin,
+      direction,
+    }
+  }
+
+  pub fn at(self, t: f64) -> Point3 {
+    return self.origin + self.direction * t;
+  }
+}
+
+
+#[cfg(test)]
+mod test {
+  use super::Ray;
+  use super::Vec3;
+  use super::Point3;
+
+  #[test]
+  fn test_ray() {
+    let r = Ray::new(Point3::new(1.0, 1.0, 1.0), Vec3::new(3.0, 4.0, 0.0));
+    assert_eq!(r.origin, Point3::new(1.0, 1.0, 1.0));
+  }
+
+  #[test]
+  fn test_at() {
+    let r = Ray::new(Point3::new(3.0, 4.0, 5.0), Vec3::new(1.0, 1.0, 1.0));
+    assert_eq!(r.at(2.0), Point3::new(5.0, 6.0, 7.0));
+  }
 }
