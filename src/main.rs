25baa3cbddc6c69b694cc0798e00cd3918d2d53d
jiazhu <garfield@bupt.edu.cn>
2020-07-11 21:13:40 +0800

    doc: add samples

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use raytracing::misc::samples;
 
 fn main() {
+  // Chapter 2: output an basic image
   // samples::outputImage();
+
+  // Chapter 4, 5: ray, sphere
   samples::ray_to_scene();
+
 }
