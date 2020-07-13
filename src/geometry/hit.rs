3823922535a2c11915a7a49036a1b288ca663a80
jiazhu <garfield@bupt.edu.cn>
2020-07-13 23:38:07 +0800

    feat: add hittable list

--- src/geometry/hit.rs
+++ src/geometry/hit.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Formatter};
+
 use crate::core::{
   point3::Point3,
   ray::Ray,
@@ -13,4 +15,17 @@ pub struct HitRecord {
 
 pub trait Hittable {
   fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, record: &mut HitRecord) -> bool;
+}
+
+pub struct HittableList {
+  objects: Vec<Box<dyn Hittable>>,
+}
+
+impl Debug for HittableList {
+  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+      f.write_fmt(format_args!(
+          "HittableList: {{ objects: {}}}",
+          self.objects.len()
+      ))
+  }
 }
