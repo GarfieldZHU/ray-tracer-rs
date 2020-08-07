82d2e61b8a41a198c96c9b6a550d06cb4dc9a258
jiazhu <jiazhu@microstrategy.com>
2020-08-07 11:24:13 +0800

    feat: add metal

---
+++ src/materials/metal.rs
@@ -0,0 +1,22 @@
+use crate::core::{
+  color::Color,
+  ray::Ray,
+  vec3::Vec3,
+};
+use crate::geometry::{
+  hit::HitRecord,
+};
+use super::Material;
+
+pub struct Metal {
+  pub albedo: Color,
+}
+
+impl Material for Metal {
+  fn scatter(self, r_in: &Ray, record: &HitRecord, attenuation: &Color, scattered: &Ray) -> bool {
+    let reflected = Vec3::reflect(r_in.direction.unit(), record.normal);
+    let scattered = Ray::new(record.point, reflected);
+    // attenuation = self.albedo;
+    Vec3::dot(&scattered.direction, &record.normal) > 0.0
+  }
+}
