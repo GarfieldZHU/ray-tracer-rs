bf93ecddae3b7126168b31708fac23678b6057f2
jiazhu <garfield@bupt.edu.cn>
2020-08-18 00:05:02 +0800

    fix: use total internal reflection

--- src/materials/dielectric.rs
+++ src/materials/dielectric.rs
@@ -22,11 +22,25 @@ impl Material for Dielectric {
   fn scatter(&self, r_in: &Ray, record: &HitRecord) -> Option<ScatterRecord> {
     let etai_over_etat: f64 = if record.front_face { 1.0 / self.ref_idx } else { self.ref_idx };
     let unit_direction: Vec3 = r_in.direction.unit();
-    let refracted: Vec3 = Vec3::refract(unit_direction, record.normal, etai_over_etat);
 
-    Some(ScatterRecord {
-      attenuation: Color::new(1.0, 1.0, 1.0),
-      scattered: Ray::new(record.point, refracted),
-    })
+    let cos_theta: f64 = Vec3::dot(&(-unit_direction), &record.normal).min(1.0);
+    let sin_theta: f64 = (1.0 - cos_theta * cos_theta).sqrt();
+    if etai_over_etat * sin_theta > 1.0 {
+      // Reflection
+      let reflected: Vec3 = Vec3::reflect(unit_direction, record.normal);
+      Some(ScatterRecord{
+        attenuation: Color::new(1.0, 1.0, 1.0),
+        scattered: Ray::new(record.point, reflected),
+      })
+    } else {
+      // Refraction
+      let refracted: Vec3 = Vec3::refract(unit_direction, record.normal, etai_over_etat);
+      Some(ScatterRecord {
+        attenuation: Color::new(1.0, 1.0, 1.0),
+        scattered: Ray::new(record.point, refracted),
+      })
+    }
+
+    
   }
 }
--- src/misc/samples.rs
+++ src/misc/samples.rs
@@ -131,9 +131,9 @@ pub fn ray_to_scene_advance(scene: AdvanceSceneCase) {
           world.add(Sphere::new(Point3::new( 1.0, 0.0, -1.0), 0.5, material_right));
         } else if scene == AdvanceSceneCase::RefractionScene  {
           let material_ground = Lambertian { albedo: Color::new(0.8, 0.8, 0.0) };
-          let material_center = Dielectric::new(1.5);
+          let material_center = Lambertian { albedo: Color::new(0.1, 0.2, 0.5) };
           let material_left = Dielectric::new(1.5);
-          let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 1.0);
+          let material_right = Metal::new(Color::new(0.8, 0.6, 0.2), 0.0);
 
           world.add(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground));
           world.add(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_center));
