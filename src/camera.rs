46e5844c39a76fca45f32a388a68c826e85c9a21
jiazhu <garfield@bupt.edu.cn>
2020-08-20 23:15:45 +0800

    feat: add camera viewing angle

--- src/camera.rs
+++ src/camera.rs
@@ -2,6 +2,7 @@ use crate::core::{
   point3::Point3,
   vec3::Vec3,
   ray::Ray,
+  degrees_to_radians,
 };
 
 #[derive(Clone, Copy, Debug)]
@@ -19,6 +20,21 @@ impl Camera {
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
 
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Point3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let ll = origin - horizontal/2 - vertical/2 - Vec3::new(0.0, 0.0, focal_length);
+
+    Self { origin, ll, horizontal, vertical }
+  }
+
+  pub fn new_para(vfov: f64, aspect_ratio: f64) -> Self {
+    let theta = degrees_to_radians(vfov);
+    let h = (theta / 2.0).tan();
+    let viewport_height = 2.0 * h;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+    
     let origin = Point3::new(0.0, 0.0, 0.0);
     let horizontal = Point3::new(viewport_width, 0.0, 0.0);
     let vertical = Vec3::new(0.0, viewport_height, 0.0);
